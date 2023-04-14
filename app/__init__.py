d132c61d0d2bafc9641ae36deb551469ccaee0fa
putro <putro@autistici.org>
2023-04-14 16:22:02 +0200

    added logging

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,9 @@ from .config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_user import current_user, login_required, roles_required, UserManager, UserMixin
+import logging
+logging.basicConfig(filename='logfile.log', level=logging.DEBUG, format=f'%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s')
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
