159d2ef2c5cc7df0bc0d8f5eff5d18a3c52abf1e
putro <putro@autistici.org>
2023-05-05 10:27:49 +0200

    added email validation, fk display on admin, other tables for db

--- app/models.py
+++ app/models.py
@@ -3,6 +3,7 @@ from app import login_manager
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_admin.contrib.sqla import ModelView
 from flask_login import UserMixin
+from sqlalchemy.orm import validates
 
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
@@ -18,6 +19,12 @@ class User(UserMixin, db.Model):
     alias = db.Column(db.String(3), nullable=False, unique=True)
     admin = db.Column(db.Boolean(), nullable=False, default=False)
     superadmin = db.Column(db.Boolean(), nullable=False, default=False)
+
+    @validates('email')
+    def validate_email(self, key, address):
+        if '@' not in address:
+            raise ValueError("Failed simple email validation")
+        return address
 
     def __init__(self, email, password, alias):
         self.email = email
@@ -45,7 +52,8 @@ class Locations(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(128), index=True, unique=True)
     alias = db.Column(db.String(8), index=True, unique=True)
-    #reagents = db.relationship('Inventory', backref='locations')
+#    reagents = db.relationship('Inventory', backref='location')
+#    reagents = db.relationship('Inventory', backref=db.backref('location', lazy=True))
 
     def __repr__(self):
         return self.name
@@ -58,7 +66,7 @@ class Inventory(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(256), index=True, unique=False, nullable=False)
     location_id = db.Column(db.Integer, db.ForeignKey('locations.id'))
-    location = db.relationship('Locations', backref='inventory')#, lazy='select')
+    location = db.relationship('Locations', backref='location', lazy=True)
     amount = db.Column(db.Integer, default=0)
     amount2 = db.Column(db.Integer, default=0)
     amount_limit = db.Column(db.Integer, default=0)
@@ -69,8 +77,32 @@ class Inventory(db.Model):
     def __repr__(self):
         return self.name #'<Reagent {}>'.format(self.name)
 
+class Products(db.Model):
+    __tablename__ = 'products'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(256), index=True, unique=False, nullable=False)
+    location = db.Column(db.String(16), nullable=False)
+    amount = db.Column(db.Integer, default=0)
+    amount2 = db.Column(db.Integer, default=0)
+    amount_limit = db.Column(db.Integer, default=0)
+    size = db.Column(db.String(16), nullable=False)
+    notes = db.Column(db.String(512))
+    to_be_ordered = db.Column(db.Integer, default=0)
+
+    def __repr__(self):
+        return self.name #'<Reagent {}>'.format(self.name)
+
+class ProdLoc(db.Model):
+    __tablename__ = 'prodloc'
+    id = db.Column(db.Integer, primary_key=True)
+    location_id = db.Column(db.Integer, db.ForeignKey('locations.id'))
+    product_id = db.Column(db.Integer, db.ForeignKey('products.id'))
+    location = db.relationship('Locations', backref='location2', lazy=True)
+    product = db.relationship('Products', backref='product2', lazy=True)
+
+
 class InventoryView(ModelView):
-    column_display_pk = True # optional, but I like to see the IDs in the list
+    column_display_pk = True # optional, but I don't like to see the IDs in the list
     column_hide_backrefs = False
-    #column_list = ('id', 'name', 'parent')
+    column_list = ('id', 'name', 'location', 'amount', 'amount2', 'amount_limit', 'size', 'notes', 'to_be_ordered')
 
