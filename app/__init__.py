3e3f535133094490191a3cd908dde2117401211a
putro <putro@autistici.org>
2023-10-22 20:05:40 +0200

    removed old login_manager related commented row

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,9 @@
 import os
 import logging
-from flask import Flask
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_bcrypt import Bcrypt
-#from flask_login import LoginManager
 from flask.logging import create_logger
 from flask_bootstrap import Bootstrap5
 from flask_security.models import fsqla_v3 as fsqla
@@ -19,10 +18,6 @@ logw.setLevel(logging.ERROR)
 app = Flask(__name__)
 log = create_logger(app)
 app.config.from_object(Config)
-
-#login_manager = LoginManager(app)
-#login_manager.login_view = 'login'
-#login_manager.init_app(app)
 
 bootstrap = Bootstrap5(app)
 
@@ -39,8 +34,3 @@ migrate = Migrate(app, db)
 
 from app import routes, models
 from app.models import User
-
-#@login_manager.user_loader
-#def load_user(user_id):
-#    return User.query.get(user_id)
-
